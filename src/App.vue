6a9796a4de3b4b9551e56d0e839c7557ddc5c9c9
Bina 288 <bina22work@gmail.com>
2024-09-13 20:31:37 +0300

    Fix: pinch footer with flexbox

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <header-app />
-  <main>
-    <router-view />
-  </main>
-  <footer-app />
+  <div class="page-container">
+    <header-app />
+    <main class="main-content">
+      <router-view />
+    </main>
+    <footer-app />
+  </div>
 </template>
+
 <script>
 import HeaderApp from "@/components/HeaderApp.vue";
 import FooterApp from "@/components/FooterApp.vue";
+
 export default {
   components: { HeaderApp, FooterApp },
 };
 </script>
 
 <style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+}
 </style>
